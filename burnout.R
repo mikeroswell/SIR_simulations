b0ff6729b84b40c6ffb16f75c1be9727839caad2
Your Name <you@example.com>
2016-03-28 16:52:52 -0400

    Keep the old burnout as well

--- burnout.R
+++ burnout.R
@@ -1,6 +1,1 @@
-slist <- list(
-	sim(R0=2, rho=0)
-	, sim(R0=2, rho=0, x0=0.9, finTime=25)
-	, sim(R0=2, rho=0, x0=0.8, finTime=30)
-	, sim(R0=2, rho=0, x0=0.7, finTime=40)
-)
+sdat <- sim(R0=2, rho=0)
