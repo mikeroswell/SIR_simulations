a59f8d0bf6ad6dc346ce21ea819a27e8510844bd
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-04-05 11:08:04 -0400

    back from etb

--- burnout.R
+++ burnout.R
@@ -1,1 +1,4 @@
+## Consider changing this number
 sdat <- sim(R0=2, rho=0)
+## Use this one for tests; can change R0 without changing lecture
+sdat <- sim(R0=3, rho=0)
---
+++ newPlots.R
@@ -0,0 +1,7 @@
+
+slist <- list(
+	sim(R0=3, rho=0)
+	, sim(R0=3, rho=0, x0=0.9, finTime=25)
+	, sim(R0=3, rho=0, x0=0.8, finTime=30)
+	, sim(R0=3, rho=0, x0=0.7, finTime=40)
+)
