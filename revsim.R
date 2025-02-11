94f15b6845070b3b9f46e570a42fce7c40de548d
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-02-11 17:08:12 +0800

    CPH

--- revsim.R
+++ revsim.R
@@ -3,11 +3,19 @@ library(shellpipes)
 sir <- function(time, vars, parms){
 	x <- with(as.list(vars), exp(lx))
 	y <- with(as.list(vars), exp(ly))
+	Ri <- with(parms, R0*x)
+	inc <- with(parms, Ri*y)
+	Ri <- with(parms, R0)
 
-	return(with(parms, list(c(
-		lxdot = dir*(rho*(1-x)/x - R0*y),
-		lydot = dir*(R0*x-1),
-		cumdot = R0*x*y
+	return(with(c(parms, as.list(vars)), list(c(
+		lxdot = dir*(rho*(1-x)/x - R0*y)
+		, lydot = dir*(R0*x-1)
+		, Rcdot = Ri - Rc
+		, Qcdot = Ri^2 - Qc
+		, cumdot = inc
+		, cumRdot = Rc*inc
+		, cumQdot = Qc*inc
+		, cumRRdot = Rc*Rc*inc
 	))))
 }
 
@@ -17,7 +25,7 @@ sim <- function(x0=NULL, y0=0.001
 ){
 	if(is.null(x0)){x0 <- 1-y0}
 	sim <- as.data.frame(ode(
-		y=c(lx=log(x0), ly=log(y0), cum=0),
+		y=c(lx=log(x0), ly=log(y0), Rc=0, Qc=0, cum=0, cumR=0, cumQ=0, cumRR=0),
 		func=dfun,
 		times=seq(from=0, to=finTime, by=timeStep),
 		parms=list(R0=R0, rho=rho, dir=ifelse(forward, 1, -1))
@@ -27,8 +35,6 @@ sim <- function(x0=NULL, y0=0.001
 		x <- exp(lx)
 		y <- exp(ly)
 		z <- 1-x-y
-		inc <- c(NA, cum[-1] - cum[-nrow(sim)])
-		incGen <- inc/timeStep
 	}))
 }
 saveEnvironment()
--- revtest.R
+++ revtest.R
@@ -3,13 +3,26 @@ library(shellpipes)
 sourceFiles()
 loadEnvironments()
 
-sdat <- sim(R0=3, rho=0)
-qdat <- sim(R0=3.1, rho=0)
+R0 <- 12
+rho <- 0
+
+sdat <- sim(R0=R0, rho=rho)
+## print(sdat[nrow(sdat), ])
 
 rdat <- with(as.list(sdat[nrow(sdat), ]), 
-	sim(R0=3, rho=0, x0=x, y0=y, forward=FALSE)
+	sim(R0=R0, rho=rho, x0=x, y0=y, forward=FALSE)
 )
+## print(rdat[nrow(rdat), ])
 
-slist <- list(qdat, rdat)
+slist <- list(sdat, rdat)
+
+with(as.list(rdat[nrow(rdat), ]), {
+	mu <- cumR/cum
+	Qwithin <- cumQ/cum
+	Vwithin <- Qwithin - mu^2
+	Qbetween <- cumRR/cum
+	Vbetween <- Qbetween - mu^2
+	print(c(mu=mu, Vwithin=Vwithin, Vbetween=Vbetween))
+})
 
 saveEnvironment()
