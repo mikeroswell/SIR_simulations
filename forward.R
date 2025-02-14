5ae37b173fe0e18435cf2a73eccd6cae21510719
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-02-14 22:43:27 +0800

    new file:   outputs/forward.sim.Rout

--- forward.R
+++ forward.R
@@ -4,19 +4,14 @@ library(rlang)
 sourceFiles()
 loadEnvironments()
 
-## This robust solver was built without gamma (equivalent to gamma=1), which seems ok if we can remember that
-R0 <- 4
-rho <- 0
-timeStep = 0.1
-
 ## Make an environment so that I can pass things to mderivs (which is called by deSolve, so I don't know how to pass the normal way).
 ## TODO: See if there is a deSolve solution to pass extra stuff
 mfuns <- env()
 
 ## m for moment; these two functions integrate across the infectors from a given cohort
 mderivs <- function(time, vars, parms){
-	Ri <- mfuns$sfun(time)
-	dens <- with(parms, exp(-(time-T0)))
+	Ri <- parms$flist$sfun(time)
+	dens <- with(parms$plist, exp(-(time-T0)))
 	return(with(c(parms, vars), list(c(
 		Rcdot = Ri
 		, ddot = dens
@@ -31,7 +26,10 @@ cMoments <- function(times, sfun, T0){
 		y=c(Rc=0, cum=0, Rctot=0, RcSS=0)
 		, func=mderivs
 		, times=times
-		, parms=list(T0=T0)
+		, parms=list(
+			plist=list(T0=T0)
+			, flist=list(sfun=sfun)
+		)
 	))
 	return(mom)
 }
@@ -74,12 +72,17 @@ oderivs <- function(time, vars, parms){
 	))))
 }
 
-outbreakStats <- function(R0, rho=0, timeStep=0.1, maxCohort=40, buffer=25){
-	sdat <- sim(R0=R0, rho=rho, timeStep=timeStep
-		, finTime=maxCohort+buffer
+outbreakStats <- function(R0, y0=1e-3
+	, rho=0, tmult=6, cohortProp=0.6, steps=300
+){
+	rate <- (R0-1)/R0
+	finTime <- tmult*(-log(y0))/rate
+	print(finTime)
+	sdat <- sim(R0=R0, rho=rho, timeStep=finTime/steps, y0=y0
+		, finTime=finTime
 	)
 	mfuns$ifun <- approxfun(sdat$time, sdat$x*sdat$y, rule=2)
-	cStats <- cohortStats(R0, sdat, maxCohort)
+	cStats <- cohortStats(R0, sdat, cohortProp*finTime)
 	mfuns$rcfun <- approxfun(cStats$cohort, cStats$Rc, rule=2)
 	mfuns$varrcfun <- approxfun(cStats$cohort, cStats$varRc, rule=2)
 
@@ -96,8 +99,15 @@ outbreakStats <- function(R0, rho=0, timeStep=0.1, maxCohort=40, buffer=25){
 		within <- (V/cum)/mu^2
 		between <- (SS-mu^2)/mu^2
 		total <- within+between
-		return(c(within=within, between=between, total=total))
+		return(c(R0=R0, size=R0*cum, mu=mu
+			, within=within, between=between, total=total
+		))
 	})
 }
 
-outbreakStats(10)
+R0 <- c(1.2, 1.5, 2, 4, 8)
+steps <- 3e2
+
+print(as.data.frame(t(
+	sapply(R0, function(x) outbreakStats(R0=x, steps=steps))
+)))
