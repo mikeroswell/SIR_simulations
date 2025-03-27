4626febc0c072eb2aeecd274331c2def086d3e2e
mikeroswell <mike.roswell@gmail.com>
2025-03-27 11:21:06 -0400

    Playing with finer steps Autocommit

--- conjecture.R
+++ conjecture.R
@@ -2,10 +2,18 @@ library(shellpipes)
 rpcall("conjecture.Rout conjecture.R forward.sim.rda deSolve.R")
 sourceFiles()
 loadEnvironments()
-R0 <- c(1.2, 1.5, 2, 4, 8)
-steps <- 3e2
+R0 <- c(1.2, 1.5, 2, 4, 8, 20)
+steps <- c(3e2, 1e3, 3e3, 1e4)
 
-print(as.data.frame(t(
-  sapply(R0, function(x) outbreakStats(R0=x, steps=steps))
-)))
+sapply(steps, function(s){
+  print(as.data.frame(
+  t(sapply(R0, function(x) outbreakStats(R0=x, steps=s))
+    )
+  ))
+  }
+  )
+
+
+
+
 
