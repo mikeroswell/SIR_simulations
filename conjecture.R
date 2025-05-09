558142ccb0e9321fda9044766e1557708a60ea6b
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-05-09 17:11:03 -0400

    modified:   conjecture.R

--- conjecture.R
+++ conjecture.R
@@ -2,19 +2,20 @@ library(shellpipes)
 rpcall("conjecture.Rout conjecture.R forward.sim.rda deSolve.R")
 sourceFiles()
 loadEnvironments()
-R0 <- c(1.2, 1.5, 2, 4, 8, 20)
-steps <- c(3e2, 1e3, 3e3, 1e4)
+R0 <- c(1.4, 2, 4)
+steps <- c(3e2)
+rho = 0.1
+tmult = 6
+cohortProp = 0.6
 
 sapply(steps, function(s){
-  print(as.data.frame(
-  t(sapply(R0, function(x) outbreakStats(R0=x, y0 = 1e-9, steps=s))
-    )
-  ))
-  }
-  )
-
-conjectureDat <- as.data.frame(
- t(sapply(R0, function(x) outbreakStats(R0=x,  y0 = 1e-9, steps=1e4))))
-
+	print(as.data.frame(
+		t(sapply(R0, function(x)
+			outbreakStats(R0=x, y0 = 1e-9, steps=s
+				, rho=rho, tmult=tmult, cohortProp=cohortProp
+			)
+		))
+	))
+})
 
 saveEnvironment()
