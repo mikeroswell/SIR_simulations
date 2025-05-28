42eb9de954e5ae043b1aeb9aac4e51320b9e88b1
mikeroswell <mike.roswell@gmail.com>
2025-05-28 11:26:37 -0400

    I think this is just recording param values run Autocommit

--- boxCarConjecture.R
+++ boxCarConjecture.R
@@ -3,20 +3,23 @@ rpcall("boxCarConjecture.Rout boxCarConjecture.R forward.sim.rda deSolve.R")
 
 sourceFiles()
 loadEnvironments()
-R0 <- c(1.2, 1.5, 2, 4, 8, 20, 25)
 R0 <- c(1.6)
+R0 <- c(1.2, 1.5, 2, 4, 8, 20, 25)
+
 
 steps <- c(1e3)
-steps <- c(5e4)
+steps <- c(5e3)
 
-cars <- c(1:6)
 cars <- c(3)
+cars <- c(2:6)
+
 
 out <- sapply(cars,function(bxs){
-  as.data.frame(t(sapply(
-  R0, function(x) outbreakStats(R0=x, y0 = 1e-9, steps=steps, cars = bxs))
-  ))
+  x <- as.data.frame(t(sapply(
+  R0, function(x) outbreakStats(R0=x, y0 = 1e-9, steps=steps, cars = bxs))))
+  print(x)
+  return(x)
 })
 
-print(out)
+
 saveEnvironment()
