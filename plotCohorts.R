61c958a914df5c9db84df87c1713b0c100499bd5
mikeroswell <mike.roswell@gmail.com>
2025-06-11 16:03:17 -0400

    Ading incidence curves to the cohort plots, also saving environment to avoid recalculating next time Autocommit

--- plotCohorts.R
+++ plotCohorts.R
@@ -7,9 +7,15 @@ library(ggplot2)
 library(dplyr)
 library(purrr)
 library(patchwork)
+RO <- c(1.2, 1.5, 2, 4, 8)
 
-cohorts <- map_dfr(c(1.2, 1.5, 2, 4, 8), function(R0){
+cohorts <- map_dfr(RO, function(R0){
   return(data.frame(sapply(cohortStats(R0, steps = 1e4, y0 = 1e-9, cars = 1), unlist), R0 = R0))
+}
+)
+
+straightSim <- map_dfr(RO, function(R0){
+  return(data.frame(simWrap(R0, steps = 1e4, y0 = 1e-9, cars = 1, tmult = 6)["sdat"], R0 = R0))
 }
 )
 
@@ -52,4 +58,21 @@ kc <- cohorts |>
         , legend.position.inside = c(0.75, 0.4))
 
 
-rc + scale_y_continuous(limits = c(0,9), breaks = seq(0,8,2)) + kc + xlim(c(0, 150))
+epiPlot <- straightSim |>
+  ggplot(aes(sdat.time, sdat.inc, color = as.factor(R0))) +
+  # geom_line(color = "black")+
+  # geom_line(aes(y = sdat.x), color = "black") +
+  # geom_line(aes(y = sdat.y), color = "black") +
+  geom_line(linewidth = 0.7)+ #, color = "red")+
+  #geom_line(aes(y = sdat.cum), linewidth = 0.7, color = "darkorange") +
+  # geom_line(aes(y = sdat.y), linewidth = 0.7, color = "darkred") +
+  # facet_wrap(~R0, scales = "free_x") +
+  scale_color_viridis_d()+
+  labs(x = "time", y = "incidence", color = "R0") +
+  xlim(c(0,150))+
+  theme_classic()
+
+
+rc + scale_y_continuous(limits = c(0,9), breaks = seq(0,8,2)) + kc + xlim(c(0, 150)) + epiPlot
+
+saveEnvironment()
